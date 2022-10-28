b844d73cb5d86cfc6d40901370037cc90b2e97bd
CaioNunesS <caiogrb@gmail.com>
2022-10-28 00:50:41 -0300

    Aplicando o darkMode

---
+++ aula5/index.js
@@ -0,0 +1,17 @@
+let darkMode = document.querySelector('body').classList.add('darkMode');
+console.log(darkMode);
+
+// console.log(confirmacao);
+let dark = document.querySelector('.darkMode');
+// console.log(dark);
+
+let confirmacao = confirm('Deseja alterar para o tema escuro?')
+if(confirmacao === false){
+    dark.classList.toggle('darkMode')
+}
+
+
+
+
+
+
